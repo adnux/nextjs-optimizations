41b8f95012d77f3b8ecf3d006a1eb1faea5911be
Andre Ferreira <adnux182@gmail.com>
2024-09-24 17:12:05 -0300

    Async metadata

--- app/page.js
+++ app/page.js
@@ -3,6 +3,15 @@ import { Suspense } from 'react';
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+export async function generateMetadata() {
+  const posts = await getPosts(2);
+  const numberOfPosts = posts?.length;
+  return {
+    title: `NextPosts - Latest ${numberOfPosts} posts`,
+    description: 'Browse all the posts on the blog.',
+  };
+}
+
 async function LatestPosts() {
   const latestPosts = await getPosts(2);
   return <Posts posts={latestPosts} />;
@@ -14,9 +23,9 @@ export default async function Home() {
       <h1>Welcome back!</h1>
       <p>Here's what you might've missed.</p>
       <section id="latest-posts">
-      <Suspense fallback={<p>Loading recent posts...</p>}>
-        <LatestPosts />
-      </Suspense>
+        <Suspense fallback={<p>Loading recent posts...</p>}>
+          <LatestPosts />
+        </Suspense>
       </section>
     </>
   );
